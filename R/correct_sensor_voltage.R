0e295467232557443b6c238c06c5336f68883339
Jakob Terschlüsen <terschluesen@uni-potsdam.de>
2023-04-12 12:24:55 +0200

    correct quantile selection (min <-> max)

--- R/correct_sensor_voltage.R
+++ R/correct_sensor_voltage.R
@@ -40,9 +40,9 @@ correct_sensor_voltage = function(V, serial_no=NULL, probe_id=NULL, ring_no=1, c
     unique_settings[ss, "type"] = tt$type
   
     #check consistency between maximum recorded voltages and calibration voltage for water
-    V_range_measured = quantile(V[cur_rows], probs = c(0.01, 0.999), na.rm=TRUE) #get range of voltage measured, discarding outliers
-    Vmax_measured = V_range_measured[1] #get maximum voltage measured, discarding outliers
-    Vmin_measured = V_range_measured[2] #get minimum voltage measured, discarding outliers
+    V_range_measured = quantile(V[cur_rows], probs = c(0.01, 0.99), na.rm=TRUE) #get range of voltage measured, discarding outliers
+    Vmax_measured = V_range_measured[2] #get maximum voltage measured, discarding outliers
+    Vmin_measured = V_range_measured[1] #get minimum voltage measured, discarding outliers
     
     #prepare warning message
     probe_id_str = paste0(ifelse(is.null(unique_settings$probe_id), paste0("ser_no '",unique_settings$serial_no[ss]), paste0("probe-id '", unique_settings$probe_id[ss])),"', ring '", unique_settings$ring_no[ss])
