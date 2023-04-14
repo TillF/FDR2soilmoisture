9a39f28470315e98a0daa1a86c42c05a2f159f0a
Jakob Terschlüsen <terschluesen@uni-potsdam.de>
2023-04-14 12:54:27 +0200

    change fucntion of V_corr to var_corr, eich can be applied also to Permittivity

--- R/V_corr.R
+++ R/V_corr.R
@@ -1,8 +1,40 @@
 #linear correction of sensor voltage using measured voltages in air and water
+
+#V_corr = function(V, V_air_meas, V_h2o_meas, type)
+#{
+#  V_air_nominal = eps2V(eps_air, type) #theoretical value for voltage in air [V]
+#  V_h2o_nominal = eps2V(eps_h2o, type) #theoretical value for voltage in water  [V]
+#  
+#  V_corr = (V_h2o_nominal - V_air_nominal) / (V_h2o_meas - V_air_meas) * (V - V_air_meas) + V_air_nominal
+#  return(V_corr)
+#}
+
+# add function for different inputs
+#here the function should depend on the input variable, e.g. counts/perm/etc
+var_corr = function(x, var_air_meas, var_h2o_meas, type, var_type)
+{
+  #here the function should depend on the input variable, e.g. counts/perm/etc
+  if (var_type=="Voltage"){
+    var_air_nominal = eps2V(eps_air, type) #theoretical value for voltage in air [V]
+    var_h2o_nominal = eps2V(eps_h2o, type) #theoretical value for voltage in water [V]
+  } else if(var_type=="Permittivity"){
+    var_air_nominal = eps_air #theoretical value for epsilon in air
+    var_h2o_nominal = eps_h2o #theoretical value for epsilon in water
+  } else{
+    var_air_nominal = NA #theoretical value for epsilon in air
+    var_h2o_nominal = NA #theoretical value for epsilon in water
+    warning("Only Voltage and Permittivity are yet implemented, giving NA for other input variables")
+  }
+    
+  var_corr = (var_h2o_nominal - var_air_nominal) / (var_h2o_meas - var_air_meas) * (x - var_air_meas) + var_air_nominal
+  return(var_corr)
+}
+
+#implementing older function:
+
 V_corr = function(V, V_air_meas, V_h2o_meas, type)
 {
-  V_air_nominal = eps2V(eps_air, type) #theoretical value for voltage in air [V]
-  V_h2o_nominal = eps2V(eps_h2o, type) #theoretical value for voltage in water  [V]
-  V_corr = (V_h2o_nominal - V_air_nominal) / (V_h2o_meas - V_air_meas) * (V - V_air_meas) + V_air_nominal
+  V_corr = var_corr(x = V, var_air_meas = V_air_meas, var_h2o_meas = V_h2o_meas, type = type, var_type = "Voltage")
   return(V_corr)
 }
+
