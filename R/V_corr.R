d8a30832b7591e641a249618fb4a42e3e5e987f9
Jakob Terschlüsen <terschluesen@uni-potsdam.de>
2023-08-22 17:09:01 +0200

    Formatting of manuals and improvement of documentations

--- R/V_corr.R
+++ R/V_corr.R
@@ -3,7 +3,7 @@
 #function depend on the input variable, e.g. counts/perm/etc
 var_corr = function(x, var_air_meas, var_h2o_meas, type, var_type, ...)
 {
-  # processing additional arguments
+  # processing additional arguments for SMT100
   alpha_set <- -0.1305
   beta_set <- 0.2549
   gamma_set <- 1.8342
@@ -18,14 +18,18 @@ var_corr = function(x, var_air_meas, var_h2o_meas, type, var_type, ...)
   }
   
   
-  #here the function should depend on the input variable, e.g. counts/perm/etc
+  #here the function dependa on the input variable, e.g. counts/perm/etc
   if (var_type=="Voltage"){
     var_air_nominal = eps2V(eps_air, type) #theoretical value for voltage in air [V]
     var_h2o_nominal = eps2V(eps_h2o, type) #theoretical value for voltage in water [V]
     
   } else if(var_type=="Permittivity"){
+    
     var_air_nominal = eps_air #theoretical value for epsilon in air
     var_h2o_nominal = eps_h2o #theoretical value for epsilon in water
+    
+    # add possible Temperature correction
+    #var_h2o_nominal = eps_water(T=20, equ ="Weast86")
     
   } else if(var_type=="Counts"){
     var_air_nominal = eps2counts(perm = eps_air, alpha = alpha_set, beta = beta_set, gamma = gamma_set) #theoretical value for epsilon in air
@@ -36,8 +40,12 @@ var_corr = function(x, var_air_meas, var_h2o_meas, type, var_type, ...)
     var_h2o_nominal = NA #theoretical value for epsilon in water
     warning("Only Voltage, Permittivity and Counts are yet implemented, giving NA for other input variables.\n")
   }
-    
+  
+  # Apply a correction function to correct the measured voltage var_raw of the air (var_air_meas) and water (var_h2o_meas)
+  # to the expected "should-be" ones (var_air_exp , V_h2o_exp ). Using the linear interpolation graph between those points
+  
   var_corr = (var_h2o_nominal - var_air_nominal) / (var_h2o_meas - var_air_meas) * (x - var_air_meas) + var_air_nominal
+  #return correcte values
   return(var_corr)
 }
 
