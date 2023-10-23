2c5b34b37d39471aac5450cfa7a4180b8137f879
Jakob Terschlüsen <terschluesen@uni-potsdam.de>
2023-10-23 10:58:42 +0200

    Incorporate temperature correction in V_corr

--- R/V_corr.R
+++ R/V_corr.R
@@ -1,7 +1,7 @@
 #linear correction of sensor values using measured voltages in air and water
 
 #function depend on the input variable, e.g. counts/perm/etc
-var_corr = function(x, var_air_meas, var_h2o_meas, type, var_type, ...)
+var_corr = function(x, var_air_meas, var_h2o_meas, type, var_type, temp, ...)
 {
   # processing additional arguments for SMT100
   alpha_set <- -0.1305
@@ -18,22 +18,47 @@ var_corr = function(x, var_air_meas, var_h2o_meas, type, var_type, ...)
   }
   
   
-  #here the function dependa on the input variable, e.g. counts/perm/etc
+  #processing reference values for  permittivity with temperature correction for water
+  eps_0 <- eps_air
+  eps_1 <- eps_h2o
+  
+  if(is.na(temp)){
+    temp = 20
+    warning("Since no temperature \"Temp\" is given in calib_data, permittivity of water at a temperature of 20 degree is used as default\n")
+  }
+  
+  #use iapws equation if package is installed
+  if(!require("iapws", quietly = TRUE)){
+    eps_1 <- eps_water(T=temp, equ ="Weast86")
+    warning("eps_water(T=20, equ =\"Weast86\") is used to calculate the temperature dependece of epsilon.
+            Consider the installation of the iapws package (see ?eps_water) or the usage of individual permittivity reference values (see ?correct_sensor_values).")
+  }else{
+    eps_1 <- eps_water(T=temp, equ ="iapws")
+  }
+  
+  if (!is.null(list(...)$epsilon_0)) {
+    eps_0 <- list(...)$epsilon_0
+    #print("You are using your own reference values")
+    }
+  if (!is.null(list(...)$epsilon_1)) {
+    eps_1 <- list(...)$epsilon_1
+    }
+  
+
+  
+  #here the function depend on the input variable, e.g. counts/perm/etc
   if (var_type=="Voltage"){
-    var_air_nominal = eps2V(eps_air, type) #theoretical value for voltage in air [V]
-    var_h2o_nominal = eps2V(eps_h2o, type) #theoretical value for voltage in water [V]
+    var_air_nominal = eps2V(eps_0, type) #theoretical value for voltage in air [V]
+    var_h2o_nominal = eps2V(eps_1, type) #theoretical value for voltage in water [V]
     
   } else if(var_type=="Permittivity"){
     
-    var_air_nominal = eps_air #theoretical value for epsilon in air
-    var_h2o_nominal = eps_h2o #theoretical value for epsilon in water
-    
-    # add possible Temperature correction
-    #var_h2o_nominal = eps_water(T=20, equ ="Weast86")
+    var_air_nominal = eps_0 #theoretical value for epsilon in air
+    var_h2o_nominal = eps_1 #theoretical value for epsilon in water
     
   } else if(var_type=="Counts"){
-    var_air_nominal = eps2counts(perm = eps_air, alpha = alpha_set, beta = beta_set, gamma = gamma_set) #theoretical value for epsilon in air
-    var_h2o_nominal = eps2counts(perm = eps_h2o, alpha = alpha_set, beta = beta_set, gamma = gamma_set) #theoretical value for epsilon in water
+    var_air_nominal = eps2counts(perm = eps_0, alpha = alpha_set, beta = beta_set, gamma = gamma_set) #theoretical value for epsilon in air
+    var_h2o_nominal = eps2counts(perm = eps_1, alpha = alpha_set, beta = beta_set, gamma = gamma_set) #theoretical value for epsilon in water
     
   } else{
     var_air_nominal = NA #theoretical value for epsilon in air
@@ -45,7 +70,7 @@ var_corr = function(x, var_air_meas, var_h2o_meas, type, var_type, ...)
   # to the expected "should-be" ones (var_air_exp , V_h2o_exp ). Using the linear interpolation graph between those points
   
   var_corr = (var_h2o_nominal - var_air_nominal) / (var_h2o_meas - var_air_meas) * (x - var_air_meas) + var_air_nominal
-  #return correcte values
+  #return corrected values
   return(var_corr)
 }
 
